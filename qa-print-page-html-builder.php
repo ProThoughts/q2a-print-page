7ec2180726f9e59baec213db7cc1816516235aa1
yaimapp <gmonday730-mbp@yahoo.co.jp>
2018-05-30 15:28:55 +0900

    アバターの非表示などカスタマイズ

--- qa-print-page-html-builder.php
+++ qa-print-page-html-builder.php
@@ -3,12 +3,13 @@
 class print_page_html_builder
 {
 
-    public static function head_print($themeobj)
+    public static function head_print($theme_obj)
     {
         $template_path = PRINT_DIR.'/html/head.html';
-        $charset = $themeobj->content['charset'];
+        $charset = $theme_obj->content['charset'];
         $site_url = qa_opt('site_url');
-        $corecss = $themeobj->rooturl.$themeobj->css_name();
+        $corecss = $theme_obj->rooturl.$theme_obj->css_name();
+        $customcss = PRINT_RELATIVE_PATH.'/css/print-page.css';
         include $template_path;
     }
 
@@ -30,6 +31,69 @@ class print_page_html_builder
         $footer_copy = qa_lang_html('material_lite_lang/footer_copy_rights');
         $template_path = PRINT_DIR.'/html/footer.html';
         include $template_path;
+    }
+
+    public static function post_avatar_meta($theme_obj, $post, $class, $avatarprefix = null, $metaprefix = null, $metaseparator = '<br/>')
+    {
+        if($class === "qa-q-item") {
+            // delete post update meta
+            // 投稿が編集された時などに以下の情報が含まれている
+            $activity_info = array('when_2', 'who_2', 'what_2');
+            foreach($activity_info as $key) {
+                if (array_key_exists($key, $post)) {
+                unset($post[$key]);
+                }
+            }
+
+            $theme_obj->output('<nav class="'.$class.'-avatar-meta flex-style">');
+            $theme_obj->post_meta($post, $class, $metaprefix, $metaseparator);
+            $theme_obj->output('</nav>');
+
+        } else {
+            //コメントリストの場合はflex-styleで囲む
+            $c_item_class = ($class === "qa-c-item") ? 'flex-style' :  '' ;
+
+            $post_meta_show = true;
+            $hide_post_meta = array('qa-q-view', 'qa-a-item', 'qa-c-item', 'blog-widget');
+            if(in_array($class, $hide_post_meta)) {
+                $post_meta_show = false;
+            }
+
+            if($post_meta_show) {
+                $theme_obj->post_meta($post, $class, $metaprefix, $metaseparator);
+            }
+
+            $userId = $post['raw']['userid'];
+            $handle = $post['raw']['handle'];
+            if (!isset($userId)) {
+                return;
+            }
+
+            $profile_show_views = array('qa-q-view', 'qa-a-item', 'qa-c-item', 'blog-widget');
+            if(!in_array($class, $profile_show_views)) {
+                return;
+            }
+            if(qa_theme_utils::is_edit($theme_obj->content)) {
+                return;
+            }
+
+            $profileItems = qa_theme_utils::getUserprofile($userId);
+            self::displayUserprofile($theme_obj, $post, $profileItems, $userId, $handle, $class);
+        }
+
+    }
+
+    private static function displayUserprofile($theme_obj, $post,$profileItems, $userId, $handle = null, $class = null)
+    {
+        //プロフィール表示
+        $theme_obj->output('<div class="profile">');
+        $theme_obj->output('<div class="mdl-typography--subhead">');
+        $theme_obj->post_meta_who($post, 'meta');
+        //活動場所を表示
+        $location = !empty($profileItems['location']) ? $profileItems['location'] : '';
+        $theme_obj->output('<span class="mdl-chip__text"><span class="mdl-typography--font-bold">'.qa_lang_html('material_lite_lang/user_location').'</span>：'.$location.'</span>');
+        $theme_obj->output('</div>');
+        $theme_obj->output('</div>');
     }
 
 }
--- qa-print-page-layer.php
+++ qa-print-page-layer.php
@@ -76,7 +76,7 @@ class qa_html_theme_layer extends qa_html_theme_base
     public function title() {
         if ($this->template === 'print') {
             $this->output(
-                '<div class="mdl-chip mdl-cell--4-col-phone mdl-cell mdl-typography--text-center mdl-color--primary">',
+                '<div class="mdl-chip mdl-cell mdl-color--primary">',
                 '<span class="mdl-chip__text mdl-color-text--white">',
                 @$this->content['q_view']['raw']['categoryname'],
                 '</span>',
@@ -89,4 +89,87 @@ class qa_html_theme_layer extends qa_html_theme_base
         }
     }
 
+    public function q_view_main($q_view)
+    {
+        if ($this->template === 'print') {
+            
+            $this->output('<div class="qa-q-view-main">');
+
+            $this->output('<div class="mdl-navigation flex-style">');
+            $this->post_avatar_meta($q_view, 'qa-q-view');
+            $this->output('</div>');
+            $suffix=isset($q_view['when']['suffix']) ? $q_view['when']['suffix'] : '';
+            $this->output('<div class="mdl-color-text--grey-400 margin--bottom-16px margin--top-16px">');
+            $this->output(qa_lang_html('material_lite_lang/post_date'));
+            $this->output($q_view['when']['data'].$suffix);
+            $this->output(',  ');
+            $this->view_count($q_view);
+            $this->output('</div>');
+            $this->q_view_content($q_view);
+            $this->q_view_extra($q_view);
+            $this->q_view_follows($q_view);
+            $this->q_view_closed($q_view);
+            $this->post_tags($q_view, 'qa-q-view');
+
+            if (count($q_view['c_list']['cs']) > 0) {
+                $this->c_list(@$q_view['c_list'], 'qa-q-view');
+            }
+
+            $this->output('</div> <!-- END qa-q-view-main -->');
+        } else {
+            qa_html_theme_base::q_view_main($q_view);
+        }
+    }
+
+    public function avatar($item, $class, $prefix=null)
+    {
+        if ($this->template !== 'print') {
+            qa_html_theme_base::avatar($item, $class, $prefix);
+        }
+    }
+
+    public function q_view_buttons($q_view)
+    {
+        if ($this->template !== 'print') {
+            qa_html_theme_base::q_view_buttons($q_view);
+        }
+    }
+
+    public function post_avatar_meta($post, $class, $avatarprefix=null, $metaprefix=null, $metaseparator='<br/>')
+    {
+        if ($this->template === 'print') {
+            print_page_html_builder::post_avatar_meta($this, $post, $class, $avatarprefix, $metaprefix, $metaseparator);
+        } else {
+            qa_html_theme_base::post_avatar_meta($post, $class, $avatarprefix, $metaprefix, $metaseparator);
+        }
+    }
+
+    public function post_meta_who($post, $class)
+    {
+        if ($this->template === 'print') {
+            $post['who']['data'] = $post['raw']['handle'];
+        }
+        qa_html_theme_base::post_meta_who($post, $class);
+    }
+
+    public function post_tags($post, $class)
+    {
+        if ($this->template !== 'print') {
+            qa_html_theme_base::post_tags($post, $class);
+        }
+    }
+
+    public function a_item_buttons($a_item)
+    {
+        if ($this->template !== 'print') {
+            qa_html_theme_base::a_item_buttons($a_item);
+        }
+    }
+
+    public function c_item_buttons($c_item)
+    {
+        if ($this->template !== 'print') {
+            qa_html_theme_base::c_item_buttons($c_item);
+        }
+    }
 }
